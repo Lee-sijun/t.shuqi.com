41de118cf7fc90ff4c728e3121908d737d0449a6
lisijun <1583050473@qq.com>
2019-09-09 20:02:45 +0800

    添加header组件

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="header">
-    <img src="http://c-shuqi.11222.cn/assets/backIcon_ba492d1.png" alt="">
-    <div class="title">书旗</div>
-    <img src="http://c-shuqi.11222.cn/assets/home_48a04f5.png" alt="">
+    <a href="#" class="return"></a>
+    <div class="header-title">书旗</div>
+    <a href="#" class="skipHome"></a>
   </div>
 </template>
 <script>
-
+export default {
+  name: 'Header'
+}
 </script>
-
 <style scoped lang="scss">
-  .title{
-    color: #333;
-    font-size: .32rem;
+  .header{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    .header-title{
+      color: #333;
+      font-size: 18px;
+    }
+    .return{
+      display: block;
+      width: 25px;
+      height: 25px;
+      background: url("http://c-shuqi.11222.cn/assets/backIcon_ba492d1.png") no-repeat;
+      background-size: cover;
+    }
+    .skipHome{
+      display: block;
+      width: 25px;
+      height: 25px;
+      background: url("http://c-shuqi.11222.cn/assets/home_48a04f5.png") no-repeat;
+      background-size: cover;
+    }
   }
 </style>
